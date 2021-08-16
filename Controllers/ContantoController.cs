1cccd88cfa683ff0184432f64799335cb94444e1
Kevin Ruiz <capin.kevin@gmail.com>
2021-08-16 11:53:50 -0500

    actualizacion

---
+++ Controllers/ContantoController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using demomvc.Models;
+
+
+namespace demomvc.Controllers
+{
+    public class ContactoController:Controller
+    {
+        
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+
+
+
+    }
+    
+}
---
+++ Models/Contacto.cs
@@ -0,0 +1,24 @@
+namespace demomvc.Models
+{
+    public class Contacto{
+
+       public string Correo {
+           get;
+           set;
+       }
+       public string Telefono {
+           get;
+           set;
+       }
+       public string Nombre {
+           get;
+           set;
+       }
+       public string Comentarios {
+           get;
+           set;
+       }
+
+
+    }
+}
