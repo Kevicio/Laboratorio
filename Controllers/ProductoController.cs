9d3849ee632f5172e9e21584506c4bda464f5f9a
Kevin Ruiz <capin.kevin@gmail.com>
2021-08-17 12:58:42 -0500

    ultima version

--- Controllers/ProductoController.cs
+++ Controllers/ProductoController.cs
@@ -15,7 +15,11 @@ namespace demomvc.Controllers{
     public IActionResult Vender(Producto objproducto){
         double operacion1=0.0;
         double operacion2=0.0;
-        double igv=1.18;
+        double operacion3=0.0;
+        double igv=0.18;
+        double red1,red2;
+      
+        
         
         if(objproducto.cantidad== 0){
             ViewData["Message"] = "No se puede ingresar 0 como cantidad";
@@ -24,22 +28,21 @@ namespace demomvc.Controllers{
             ViewData["Message"] = "No existe ningun producto con precio 0";
         }
         else{ 
+        ViewData["Producto"] =  "Producto: "+objproducto.nombre;
         ViewData["Cantidad"] = "Cantidad Total: "+objproducto.cantidad+" unidades";
-        ViewData["Precio"] = "Precio: "+objproducto.precio;
-        ViewData["IGV"] = "IGV = 18% ";
-
+        ViewData["Precio"] = "Precio: S/."+objproducto.precio;
+        
         operacion1=objproducto.cantidad*objproducto.precio;
         ViewData["subtotal"] ="El subtotal de la venta es: S/."+operacion1;
 
-        operacion2=operacion1*igv;
+        operacion3=operacion1*igv;
+        ViewData["IGV"] = "IGV: S/."+operacion3;
+        
+        operacion2=operacion1*(1+igv);
         ViewData["Message"] ="El total de la venta es: S/."+operacion2;
 
         }
-
-
-
-
-
+       
 
 
         return View("Index");
